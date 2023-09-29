d78afc8d05b709ce785c03f1d0a630af4e2b687a
ViktarTilichkin <tilichkin@mail.ru>
2023-09-29 17:33:08 +0300

    update

---
+++ Task5/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Itra_Task5;
+
+public enum Region
+{
+    Poland,
+    Germany,
+    UnitedStates
+}
+public class PersonModel
+{
+    public int Index { get; set; }
+    public int RandomID { get; set; }
+    public string FullName { get; set; } = String.Empty;
+    public string Address { get; set; } = String.Empty;
+    public string Number { get; set; } = String.Empty;
+ }
